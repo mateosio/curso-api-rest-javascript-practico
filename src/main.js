0dfa0507ab11fe955f0382dea30d8eb7f50ba568
Mateo Sio <siomateo8@gmail.com>
2023-05-07 12:24:55 -0300

    El primer commit del curso terminado

---
+++ src/main.js
@@ -0,0 +1,285 @@
+const API = axios.create({
+    baseURL: "https://api.themoviedb.org/3/",
+    headers: {
+        "Content-Type": "application/json;charset=utf-8",
+    },
+    params: {
+        "api_key": API_KEY,
+    },
+});
+
+//Util
+const lazyLoader = new IntersectionObserver((entries)=>{
+    entries.forEach((entry)=>{
+        if(entry.isIntersecting){
+           const Url = entry.target.getAttribute("data-img");
+           entry.target.setAttribute("src", Url);  
+        }
+    })
+});
+
+function likedMovieList() {
+    const item = JSON.parse(localStorage.getItem("liked_movies"));
+    let movies;
+
+    if(item){
+        movies = item; 
+    } else{
+        movies = {};
+    }
+    return movies;
+};
+
+function likeMovie(movie){
+    const likedMovies = likedMovieList();
+    
+    if(likedMovies[movie.id]){
+        likedMovies[movie.id] = undefined;
+    }else{
+        likedMovies[movie.id] = movie;
+    };
+    localStorage.setItem("liked_movies", JSON.stringify(likedMovies));
+};
+
+
+//Función para no repetir código.
+function createMovie(movies, container, {lazyLoad = false, clean = true}={}) { 
+    if (clean){
+        container.innerHTML = "";
+    };
+
+    movies.forEach((movie)=>{
+
+        const movieContainer = document.createElement("div");
+        movieContainer.classList.add("movie-container");
+                
+        const movieImg = document.createElement("img");
+        movieImg.classList.add("movie-img");
+        movieImg.setAttribute("alt", movie.title);
+        movieImg.setAttribute(
+            lazyLoad? "data-img": "src", `https://image.tmdb.org/t/p/w300${movie.poster_path}`);
+        /*
+         Para escuchar el evento de error en una imagen y reemplazarlo por alguna otra imagen
+        movieImg.addEventListener("error", ()=>{
+        movieContainer.setAttribute("src", "url");   
+        
+        Tambien podemos eliminar la foto que genera el error para que no se vea si ponemos:
+        movieContainer.remove(movieImg);
+        });*/
+
+        movieImg.addEventListener("click", ()=>{
+            location.hash = `#movie=${movie.id}`
+        });
+
+        const movieBtn = document.createElement("button");
+        movieBtn.classList.add("movie-btn");
+        likedMovieList()[movie.id] && movieBtn.classList.add("movie-btn--liked");
+
+        movieBtn.addEventListener("click", ()=>{
+            movieBtn.classList.toggle("movie-btn--liked");
+            likeMovie(movie);
+            homePage();
+        });
+
+        
+        if(lazyLoad){
+            lazyLoader.observe(movieImg)};
+
+        movieContainer.append(movieImg, movieBtn);
+
+        container.appendChild(movieContainer);
+        
+    });
+
+};
+
+function createCategories(categories, container) {
+    container.innerHTML = "";
+
+    categories.forEach((genero)=>{
+
+        const categoryContainer = document.createElement("div");
+        categoryContainer.classList.add("category-container");
+        
+        const titleCategory = document.createElement("h3");
+        titleCategory.setAttribute("id", `id${genero.id}`);
+        titleCategory.classList.add("category-title");
+        titleCategory.addEventListener("click", ()=>{
+            location.hash = `#category=${genero.id}-${genero.name}`;
+        });
+
+        const titleText = document.createTextNode(genero.name);
+        console.log(genero.name);
+
+        titleCategory.appendChild(titleText);
+        categoryContainer.appendChild(titleCategory);
+        container.appendChild(categoryContainer);
+
+});
+};
+
+//Llamados a la API.
+async function getTrendingMoviesPreview (){
+    const { data } = await API(`trending/movie/day`);
+    const movies = data.results;
+
+    createMovie(movies, trendingMoviesPreviewList, true);
+
+    console.log({data, movies});
+};
+
+async function getCategoriesPreview (){
+    const { data } = await API(`genre/movie/list`);
+    
+    const generos = data.genres;
+
+    createCategories(generos, categoriesPreviewList);
+
+    
+};
+
+async function getMoviesByCategory (id){
+    const { data } = await API(`discover/movie/`, {
+        params: {
+            with_genres: id,
+        },
+    });
+    const movies = data.results;
+
+    createMovie(movies, genericSection, true);
+
+    console.log({data, movies});
+};
+
+async function getMoviesBySearch (query){
+    const { data } = await API(`search/movie`, {
+        params: {
+            query,
+        },
+    });
+    const movies = data.results;
+    maxPage = data.total_pages;
+
+    createMovie(movies, genericSection);
+
+    console.log({data, movies});
+};
+
+async function getTrendingMovies (){
+    const { data } = await API(`trending/movie/day`);
+    const movies = data.results;
+
+    maxPage = data.total_pages;
+
+    createMovie(movies, genericSection, {lazyLoad: true, clean: true});
+
+    
+};
+
+async function getMovieById (movieId){
+    const { data } = await API(`movie/${movieId}`);
+    
+    //Al data_averege que es la puntuación de la peli le dejo solo un decimal con el toFixed.
+    let score = data.vote_average.toFixed(1);
+    
+    movieDetailTitle.textContent = data.title;
+    movieDetailDescription.textContent = data.overview;
+    movieDetailScore.textContent = score;
+
+    createCategories(data.genres, movieDetailCategoriesList);
+
+    console.log(data);
+    const movieImgUrl = `https://image.tmdb.org/t/p/w500${data.poster_path}`;
+    headerSection.style.background = `
+    linear-gradient(
+        180deg, 
+        rgba(0, 0, 0, 0.35) 
+        19.27%, rgba(0, 0, 0, 0) 29.17%),
+    url(${movieImgUrl})`;
+    
+     getSimilarMoviesId(movieId);
+};
+
+async function getSimilarMoviesId(id) {
+     const {data} = await API(`movie/${id}/similar`)
+    const similarMovies = data.results;
+
+    createMovie(similarMovies, relatedMoviesContainer);
+ };
+
+function getPaginetedSearch (query){
+    return async function(){
+        const {scrollTop, clientHeight, scrollHeight} = document.documentElement;
+    
+        const scrollIsBottom = (scrollTop + clientHeight) >= (scrollHeight - 15);
+        const pageIsNotMax = page < maxPage;
+    
+        if(scrollIsBottom && pageIsNotMax){
+            page++;
+            const { data } = await API(`search/movie`, {
+                params: {
+                query,
+                 page,
+                },
+            });
+            
+            const movies = data.results;
+        
+            createMovie(movies, genericSection, {lazyLoad: true, clean: false});
+        };
+    };
+
+
+ };
+
+async function getPaginetedTrending (){
+    
+    const {scrollTop, clientHeight, scrollHeight} = document.documentElement;
+    
+    const scrollIsBottom = (scrollTop + clientHeight) >= (scrollHeight - 15);
+    const pageIsNotMax = page < maxPage;
+    
+    if(scrollIsBottom && pageIsNotMax){
+        page++;
+        const { data } = await API(`trending/movie/day`, {
+        params: {
+         page,
+        },
+    });
+                
+    const movies = data.results;
+            
+    createMovie(movies, genericSection, {lazyLoad: true, clean: false});
+    };  
+};
+
+function getPaginetedCategory (categoryId){
+    return async function(){
+        const {scrollTop, clientHeight, scrollHeight} = document.documentElement;
+    
+        const scrollIsBottom = (scrollTop + clientHeight) >= (scrollHeight - 15);
+        const pageIsNotMax = page < maxPage;
+    
+        if(scrollIsBottom && pageIsNotMax){
+            page++;
+            const { data } = await API(`discover/movie/`, {
+            params: {
+                with_genres: categoryId,
+                page,
+            },
+            });
+            
+            const movies = data.results;
+        
+            createMovie(movies, genericSection, {lazyLoad: true, clean: false});
+        };
+    };
+};
+
+function getLikedMovies(){
+    const likedMovies = likedMovieList();
+    console.log(likedMovies);
+    const moviesArray = Object.values(likedMovies);
+    
+    createMovie(moviesArray, likedMovieListArticle, {lazyLoad: true, clean: true});
+};
---
+++ src/navigation.js
@@ -0,0 +1,191 @@
+ let page = 1;
+ let maxPage;
+ let scrollinfinite;
+
+searchFormBtn.addEventListener("click", ()=>{
+    location.hash = `#search=${searchFormInput.value}`;
+    console.log(searchFormInput.value);
+});
+
+trendingBtn.addEventListener("click", ()=>{
+    location.hash = "#trends";
+});
+/* Si quisieramos abrir desde youtube la aplicación en un hash que no sea el home y cuando tocamos el boton atras vuelva al home en lugar de a youtube: window.addEventListener(
+    'DOMContentLoaded',
+    () => {
+        navigator();
+        // Agregando un estado de carga inical
+        window.history.pushState({ loadUrl: window.location.href }, null, '');
+    },
+    false,
+);
+arrowBtn.addEventListener("click", ()=>{
+    const stateLoad = window.history.state ? window.history.state.loadUrl : '';
+    console.log(stateLoad);
+    if (stateLoad.includes('#')) {
+        window.location.hash = '';
+        console.log("no aplica el back");
+    } else {
+        window.history.back();
+    }
+});
+*/
+arrowBtn.addEventListener("click", ()=>{
+    window.history.back();
+});
+
+window.addEventListener("DOMContentLoaded", navigator, false);
+window.addEventListener("hashchange", navigator, false);
+
+
+
+// getPaginetedTrendingMovies
+
+function navigator(){
+
+    
+    console.log({location});
+    
+    if(location.hash.startsWith("#trends")){
+        trendPage();
+
+    } else if (location.hash.startsWith("#search=")){
+        searchPage();
+
+    } else if (location.hash.startsWith("#movie=")){
+        movieDetailsPage();
+
+    }else if (location.hash.startsWith("#category=")){
+        categoriesPage();
+    } else {
+        homePage();
+    };
+    window.scrollTo(0, 0);
+    //Para que al cambiar de pagina siempre arranque la visión arriba de la pantalla.
+    
+
+     if (scrollinfinite) {
+         window.addEventListener("scroll", scrollinfinite);
+     };
+
+};
+
+function homePage(){
+    //Estas lineas de código con add y remove en realidad no hacen falta porque por defecto ya estan asi en el html, no estamos modificando nada. 
+    headerSection.classList.remove("header-container--long");
+    headerSection.style.background = "";
+    arrowBtn.classList.add("inactive");
+    arrowBtn.classList.remove("header-arrow--white");
+    headerTitle.classList.remove("inactive")
+    headerCategoryTitle.classList.add("inactive");
+    searchForm.classList.remove("inactive");
+
+    trendingPreviewSection.classList.remove("inactive");
+    categoriesPreviewSection.classList.remove("inactive");
+    likedMoviesSection.classList.remove("inactive");
+    genericSection.classList.add("inactive");
+    movieDetailSection.classList.add("inactive");
+
+    
+    getTrendingMoviesPreview();
+    getCategoriesPreview();
+    getLikedMovies();
+};
+
+
+function categoriesPage(){
+    headerSection.classList.remove("header-container--long");
+    // headerSection.style.background = "";
+    arrowBtn.classList.remove("inactive");
+    arrowBtn.classList.remove("header-arrow--white");
+    headerTitle.classList.add("inactive")
+    headerCategoryTitle.classList.remove("inactive");
+    searchForm.classList.add("inactive");
+    
+    trendingPreviewSection.classList.add("inactive");
+    categoriesPreviewSection.classList.add("inactive");
+    likedMoviesSection.classList.add("inactive");
+    genericSection.classList.remove("inactive");
+    movieDetailSection.classList.add("inactive");
+
+    const [ , categoryData] = location.hash.split("="); 
+    //Split transforma un string en un array. Luego uso la destructuración en la constante para guardar los valores de los dos elementos del array. En este caso [#category] [id-name] en vez de tener que guardar en dos constantes distintas. Como el primer array no lo uso puedo no poner ningún nombre y dejo el espacio vacio.
+    const [categoryId, categoryName] = categoryData.split("-");
+    
+    headerCategoryTitle.innerHTML = categoryName;
+
+    getMoviesByCategory(categoryId);
+    scrollinfinite = getPaginetedCategory(categoryId);
+
+};
+
+function movieDetailsPage(){
+    headerSection.classList.add("header-container--long");
+    // headerSection.style.background = "";
+    arrowBtn.classList.remove("inactive");
+    arrowBtn.classList.add("header-arrow--white");
+    headerTitle.classList.add("inactive")
+    headerCategoryTitle.classList.add("inactive");
+    searchForm.classList.add("inactive");
+    
+    trendingPreviewSection.classList.add("inactive");
+    categoriesPreviewSection.classList.add("inactive");
+    likedMoviesSection.classList.add("inactive");
+    genericSection.classList.add("inactive");
+    movieDetailSection.classList.remove("inactive");
+
+   
+    let [ , movieId] = location.hash.split("=");
+    movieId = movieId.replaceAll("%20", "");
+
+    getMovieById(movieId);
+};
+
+function searchPage(){
+    headerSection.classList.remove("header-container--long");
+    // headerSection.style.background = "";
+    arrowBtn.classList.remove("inactive");
+    arrowBtn.classList.remove("header-arrow--white");
+    headerTitle.classList.add("inactive")
+    headerCategoryTitle.classList.add("inactive");
+    searchForm.classList.remove("inactive");
+    
+    trendingPreviewSection.classList.add("inactive");
+    categoriesPreviewSection.classList.add("inactive");
+    likedMoviesSection.classList.add("inactive");
+    genericSection.classList.remove("inactive");
+    movieDetailSection.classList.add("inactive");
+
+    //Si buscamos una pelicula que tenga un nombre con más de una palabra en los espacios va a aparecer "%20" y va a darnos error. Para eliminar esto se puede usar el decodeURI o también el código de la opción comentada.
+    // let [ , query] = location.hash.split("=");
+    // query = decodeURI(query)
+    // getMoviesBySearch(query);
+    
+    let a = location.hash.split("=")[1];
+    a = a.replaceAll("%20", " ");
+
+    getMoviesBySearch(a);
+    scrollinfinite = getPaginetedSearch(a);
+
+};
+
+function trendPage(){
+    headerSection.classList.remove("header-container--long");
+    // headerSection.style.background = "";
+    arrowBtn.classList.remove("inactive");
+    arrowBtn.classList.remove("header-arrow--white");
+    headerTitle.classList.add("inactive")
+    headerCategoryTitle.classList.remove("inactive");
+    searchForm.classList.add("inactive");
+    
+    trendingPreviewSection.classList.add("inactive");
+    categoriesPreviewSection.classList.add("inactive");
+    likedMoviesSection.classList.add("inactive");
+    genericSection.classList.remove("inactive");
+    movieDetailSection.classList.add("inactive");
+
+    headerCategoryTitle.innerHTML = "Tendencias";
+    
+    getTrendingMovies();
+    scrollinfinite = getPaginetedTrending;
+};
